8f06198e088c2f5c45f8d82c06a8dd5f66ed25c1
Fresh-Bake <luqmaanbelim@gmail.com>
2026-07-27 16:06:15 +0200

    Add regression tests for invoice number extraction

---
+++ test_invoice_extraction.py
@@ -0,0 +1,48 @@
+import os
+import unittest
+
+os.environ.setdefault("GDRIVE_FOLDER_ID", "test-folder")
+os.environ.setdefault("GDRIVE_SA_JSON", "{}")
+
+from process_invoices import extract_invoice_number
+
+
+class InvoiceNumberExtractionTests(unittest.TestCase):
+    def test_new_template_ignores_registration_number(self):
+        text = (
+            "Tax Invoice Reg No: 2011/053816/23 "
+            "Tax Invoice Number INV/2026/042130"
+        )
+        self.assertEqual(extract_invoice_number(text), "2026_042130")
+
+    def test_registration_number_alone_is_not_an_invoice(self):
+        self.assertIsNone(
+            extract_invoice_number("Tax Invoice Reg No: 2011/053816/23")
+        )
+
+    def test_nv_legacy_prefix_must_be_a_separate_token(self):
+        self.assertEqual(
+            extract_invoice_number("Invoice Number NV/2026/000073"),
+            "2026_000073",
+        )
+        self.assertIsNone(
+            extract_invoice_number(
+                "Tax Invoice Reg No: 2011/053816/23 Invoice Date 26/07/2026"
+            )
+        )
+
+    def test_labelled_fallback_zero_pads_on_the_left(self):
+        self.assertEqual(
+            extract_invoice_number("Tax Invoice Number 2026/42130"),
+            "2026_042130",
+        )
+
+    def test_common_ocr_digit_substitutions(self):
+        self.assertEqual(
+            extract_invoice_number("Tax Invoice Number I N V / 2O26 / O4213O"),
+            "2026_042130",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
